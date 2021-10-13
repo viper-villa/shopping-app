44f8b39761354b4c84894114a8c1fd301523a8fd
raju-verma357 <raju@gobrilliantsolutions.com>
2021-10-13 10:06:57 +0400

    products

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
+import React from 'react';
+import Products from './components/Products';
+import data from './data.json';
 
-//feature 1
-function App() {
-  return (
-  <div className="grid-container">
-    <header>
-      <a href="/">Shopping Cart</a>
-    </header>
-    <main>
-      Product List
-    </main>
-    <footer>
-      All Right Is reserved
-    </footer>
-  </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size:"",
+      sort:""
+    };
+  }
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products}></Products>
+            </div>
+            <div className="sidebar">
+              Cart list
+            </div>
+          </div>
+        </main>
+        <footer>
+          All Right Is reserved
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
