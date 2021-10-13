6a7ccfca989f878762804aa407821a57d7eef202
raju-verma357 <raju@gobrilliantsolutions.com>
2021-10-12 18:26:13 -1200

    channges

--- src/App.js
+++ src/App.js
@@ -34,21 +34,6 @@ class App extends React.Component {
       </div>
     );
   }
-//feature-1
-function App() {
-  return (
-  <div className="grid-container">
-    <header>
-      <a href="/">Shopping Cart</a>
-    </header>
-    <main>
-      Product List
-    </main>
-    <footer>
-      All Right Is reserved
-    </footer>
-  </div>
-  );
 }
 
 export default App;
